8bd5358333547f0750790ad2637e7290c6a2d2ed
zoou2foo <zoou2foo@gmail.com>
2023-09-11 09:56:10 -0400

    added html and css

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-launchGame()
+let zoneSaisie = document.getElementById("inputEcriture");
+console.log(zoneSaisie)
+
+let buttonValidate = document.getElementById("btnValidateWord");
+console.log(buttonValidate)
+
+let zoneProposition = document.querySelector(".zoneProposition") //comme on le ferait en CSS -> class .zoneProp
+console.log(zoneProposition)
+
+let zoneScore = document.querySelector(".zoneScore span")
+console.log(zoneScore)
+
+let buttons = document.querySelectorAll(".optionSource input")
+console.log(buttons)
+
+//launchGame()
